ef1589d737bbabd708f5c3f926dfd34958d651ac
Christian Estrada <cestrada35@ucmerced.edu>
2020-05-11 15:00:45 -0700

    Basic Implementation of UI in Unit Converter app

--- Unit Conversion Calculator/Unit Conversion Calculator/ContentView.swift
+++ Unit Conversion Calculator/Unit Conversion Calculator/ContentView.swift
@@ -9,8 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputValue = ""
+    @State private var unit = 0
+    @State private var tempUnit = 0
+    @State private var lengthUnit = 0
+    
+    let unitTypes = ["Temp", "Length", "Time", "Volume"]
+    let tempUnits = ["°Celsius","°Fahrenheit","Kelvin"]
+    let lengthUnits = ["Meters","Kilometers","Feet", "Yards", "Miles"]
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            
+            
+            
+            Form {
+                Section (header: Text("Select Unit Type")){
+                    Picker("", selection: $unit) {
+                            ForEach(0 ..< unitTypes.count) {
+                                Text("\(self.unitTypes[$0])")
+                            }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section (header: Text("Select Units")){
+                    if (unit == 0){
+                        Picker("", selection: $tempUnit) {
+                                ForEach(0 ..< tempUnits.count) {
+                                    Text("\(self.tempUnits[$0])")
+                                }
+                        }.pickerStyle(SegmentedPickerStyle())
+                    }else if (unit == 1){
+                        Picker("", selection: $lengthUnit) {
+                                ForEach(0 ..< lengthUnits.count) {
+                                    Text("\(self.lengthUnits[$0])")
+                                }
+                        }.pickerStyle(SegmentedPickerStyle())
+                    }
+                    
+                    TextField("Enter Value", text: $inputValue)
+                    .keyboardType(.decimalPad)
+                }
+                
+                
+                
+            }.navigationBarTitle("Unit Conversion", displayMode: .large)
+        }
     }
 }
 
