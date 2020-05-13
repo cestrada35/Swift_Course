a5c6589dd0e1a2aae7783b3e0c8676e3a267ff9c
Christian Estrada <cestrada35@ucmerced.edu>
2020-05-12 17:34:40 -0700

    Project 2 for the swift course, the flag guessing game app(incomplete)

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Christian Estrada on 5/12/20.
+//  Copyright © 2020 Burt Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain","UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    var body: some View {
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack (spacing: 30){
+                VStack {
+                    Text("Tap the flag of:")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                    .foregroundColor(.white)
+
+                }
+                
+                ForEach(0..<3) {number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+                Spacer()
+            }
+        }
+        .alert(isPresented: $showingScore){
+            Alert(title: Text(scoreTitle), message:
+            Text("Your score is ???"),
+                  dismissButton: .default(Text("Continue")){
+                    self.askQuestion()
+                })
+        }
+    }
+    func flagTapped(_ number: Int){
+        if number == correctAnswer{
+            scoreTitle = "Correct"
+        }else{
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
