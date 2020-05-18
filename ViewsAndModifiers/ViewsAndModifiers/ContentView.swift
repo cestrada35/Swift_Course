3a4504bf09bd4637e8a0393d805b50744fc028c1
Christian Estrada <cestrada35@ucmerced.edu>
2020-05-17 21:53:52 -0700

    Testing out blur modifier

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -10,16 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-       Text("Hello World")
-       .padding()
-       .background(Color.red)
-       .padding()
-       .background(Color.blue)
-       .padding()
-       .background(Color.green)
-       .padding()
-       .background(Color.yellow)
-    }
+       VStack {
+           Text("Gryffindor")
+               .blur(radius: 1)
+           Text("Hufflepuff")
+           Text("Ravenclaw")
+           Text("Slytherin")
+       }
+       .blur(radius: 0)    }
 }
 
 struct ContentView_Previews: PreviewProvider {
