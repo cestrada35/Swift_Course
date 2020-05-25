94090a32259358c2bb411a79d46bcfd49f33bb2e
Christian Estrada <cestrada35@ucmerced.edu>
2020-05-25 09:09:31 -0700

    Created a custom watermark modifier

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -21,12 +21,36 @@ struct CapsuleText: View {
     }
 }
 
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.footnote)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
 
 struct ContentView: View {
     var body: some View {
         VStack(spacing: 10) {
             CapsuleText(text: "First")
             CapsuleText(text: "Second")
+            
+            Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
         }
     }
 }
