c08aac7471a00d791b52544358e244a460834a84
Christian Estrada <cestrada35@ucmerced.edu>
2020-05-28 19:05:54 -0700

    Clean up of User Interface

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date()
+    @State private var wakeUp = defaultWakeTime
     @State private var coffeeAmount = 1
     
     @State private var alertTitle = ""
@@ -19,11 +19,12 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
-            VStack{
+            Form {
                 Text("When do you want to wake up")
                     .font(.headline)
                 DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                     .labelsHidden()
+                    .datePickerStyle(WheelDatePickerStyle())
                 Text("Desired amount of sleep")
                     .font(.headline)
                 Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
@@ -51,6 +52,14 @@ struct ContentView: View {
             }
         }
     }
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
     func calculateBedtime() {
         let model = SleepCalculator()
         let components = Calendar.current.dateComponents([.hour,.minute], from: wakeUp)
