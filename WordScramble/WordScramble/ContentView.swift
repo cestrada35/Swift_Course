55466c6f05141f886dafd214d4a9b777eaabee14
Christian Estrada <cestrada35@ucmerced.edu>
2020-06-09 23:50:53 -0700

    Displaying user word

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -13,14 +13,32 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // exit if the remaining string is empty
+        guard answer.count > 0 else {
+            return
+        }
+
+        // extra validation to come
+
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
    var body: some View {
         NavigationView {
             VStack {
                 TextField("Enter your word", text: $newWord)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                .autocapitalization(.none)
 
                 List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
                     Text($0)
                 }
             }
@@ -34,3 +52,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
