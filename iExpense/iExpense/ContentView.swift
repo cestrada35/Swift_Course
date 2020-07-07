d3c9a3772e7b24278b2e2d28d14c8b5e01d45464
Christian Estrada <cestrada35@ucmerced.edu>
2020-07-06 17:43:43 -0700

    Added my first new view

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,20 +8,21 @@
 
 import SwiftUI
 
-struct User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+struct SecondView: View {
+    var body: some View {
+        Text("Another View")
+    }
 }
 
 struct ContentView: View {
-    @State private var user = User()
-
-    var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+@State private var showingSheet = false
+    
+   var body: some View {
+        Button("Show Sheet") {
+            self.showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView()
         }
     }
 }
